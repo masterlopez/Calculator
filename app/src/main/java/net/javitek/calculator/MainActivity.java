78951ce64bf20e859e2ec241f03c07241f4e9eaf
Javier Lopez <javy7@hotmail.com>
2016-06-21 15:04:06 -0400

    Complete App

--- app/src/main/java/net/javitek/calculator/MainActivity.java
+++ app/src/main/java/net/javitek/calculator/MainActivity.java
@@ -2,12 +2,120 @@ package net.javitek.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnDivision;
+    private Button btnMultiplication;
+    private TextView txtResult;
+    private Button btnClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        btnClear = (Button) findViewById(R.id.btnClear);
+
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0))
+                {
+                    double oper1 = Double.parseDouble(operand1.getText().toString()); // Take the text and converts to double to process.
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                    double theResult = oper1 + oper2;
+                    txtResult.setText(Double.toString(theResult)); // Takes a Double value, converts it to string and sets it as the value.
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0))
+                {
+                    double oper1 = Double.parseDouble(operand1.getText().toString()); // Take the text and converts to double to process.
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                    double theResult = oper1 - oper2;
+                    txtResult.setText(Double.toString(theResult)); // Takes a Double value, converts it to string and sets it as the value.
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0)) {
+                    double oper1 = Double.parseDouble(operand1.getText().toString()); // Take the text and converts to double to process.
+                    double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                    double theResult = oper1 / oper2;
+                    txtResult.setText(Double.toString(theResult)); // Takes a Double value, converts it to string and sets it as the value.
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    if ((operand1.getText().length() > 0) && (operand2.getText().length() > 0))
+                    {
+                        double oper1 = Double.parseDouble(operand1.getText().toString()); // Take the text and converts to double to process.
+                        double oper2 = Double.parseDouble(operand2.getText().toString());
+
+                        double theResult = oper1 * oper2;
+                        txtResult.setText(Double.toString(theResult)); // Takes a Double value, converts it to string and sets it as the value.
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Please enter numbers in both operand fields", Toast.LENGTH_LONG).show();
+                    }
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operand1.setText("");
+                operand2.setText("");
+                txtResult.setText("0.00");
+                operand1.requestFocus();
+            }
+        });
+
+
+
+
+
     }
+
 }
